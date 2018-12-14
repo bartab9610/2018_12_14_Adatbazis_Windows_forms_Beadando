0dfa528b14a87c5bad066f0ff1bc7275535d9da9
bartab9610 <barnabas96@freemail.hu>
2018-12-14 13:06:20 +0100

    Táblák létrehozása adatbázisban!

--- 2018_12_14_HarcosokApplication/Form1.cs
+++ 2018_12_14_HarcosokApplication/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace _2018_12_14_HarcosokApplication
 {
     public partial class Form_harcos_adatbazis : System.Windows.Forms.Form
     {
+        const string Adatbazis_letrehozasa_SQL = @"CREATE TABLE IF NOT EXISTS harcosok
+                                                        (
+                                                            harcos_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                                                            harcos_nev VARCHAR(64) NOT NULL UNIQUE,
+                                                            harcos_datum_letrehozas DATE NOT NULL
+                                                        );
+                                                 CREATE TABLE IF NOT EXISTS kepessegek
+                                                        (
+                                                            kepesseg_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                                                            kepesseg_nev VARCHAR(64) NOT NULL,
+                                                            kepesseg_leiras TEXT NOT NULL,
+                                                            harcos_id INTEGER NOT NULL,
+                                                            FOREIGN KEY(harcos_id) REFERENCES harcosok(harcos_id)
+                                                        )"; // JAVA-ban 1 SQL utasításban csak egy CREATE lehet vagy más!!!!!!!!!!!!!!!!!
+        MySqlConnection kapcsolodas;
+
         public Form_harcos_adatbazis()
         {
             InitializeComponent();
+            try
+            {
+                kapcsolodas = new MySqlConnection("Server=localhost;Database=cs_harcosok;Uid=root;Pwd=;");
+                kapcsolodas.Open();
+                var letrehozas = kapcsolodas.CreateCommand();
+                letrehozas.CommandText = Adatbazis_letrehozasa_SQL;
+                letrehozas.ExecuteNonQuery();
+            }
+            catch (MySqlException MySQLEx)
+            {
+                MessageBox.Show("Adatbázis hiba: " + MySQLEx.Message);
+                this.Close();
+            }
         }
     }
 }
